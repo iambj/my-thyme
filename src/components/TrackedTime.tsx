b1f0bc88d619f9dc0b1040bc2e6555f30ab397b7
iambj <iambj@icloud.com>
2021-04-08 23:20:29 -0400

    Added some icons for UX

--- src/components/TrackedTime.tsx
+++ src/components/TrackedTime.tsx
@@ -1,13 +1,28 @@
 import React from "react";
 import styled from "styled-components";
 
+import minus from "../images/minus.svg";
+import plus from "../images/plus.svg";
+import edit from "../images/edit.svg";
+
+// import { ReactComponent as Minus2 } from "../images/plus.svg";
+
 export default function TrackedTime() {
     return (
         <Time>
             <div className="number mediumText">4/8/2010</div>
             <div>Time spent working on the main CSS</div>
             <div className="number mediumText">5:30:30</div>
-            <div className="number mediumText">[ + ][ - ][ E ]</div>
+            <div>
+                <img className="button" src={plus} alt="" />
+            </div>
+            <div>
+                <img className="button" src={minus} alt="" />
+            </div>{" "}
+            <div>
+                <img className="button" src={edit} alt="" />
+            </div>
+            {/* <img className="plus" src={plus} alt="" /> */}
         </Time>
     );
 }
@@ -26,5 +41,19 @@ const Time = styled.div`
 
     div:nth-child(2) {
         flex: 1;
+        min-width: 250px;
+        margin-right: auto;
+    }
+    div {
+        /* display: flex;
+        align-self: stretch;
+        align-items: center; */
+        cursor: pointer;
+        padding: 5px;
+        display: inline;
+        img {
+            width: 1.5rem;
+            /* margin: 5px; */
+        }
     }
 `;
